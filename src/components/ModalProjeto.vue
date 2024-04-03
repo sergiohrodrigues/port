b60f6cead20e51739eeffd93f87ddff212eb8bb6
Sergio <sergio.rodrigues@mercoplasa.com.br>
2024-04-03 15:10:41 -0300

    modal centralizada

--- src/components/ModalProjeto.vue
+++ src/components/ModalProjeto.vue
@@ -6,8 +6,8 @@
         <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
       </TransitionChild>
 
-      <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-        <div class="flex h-4/4 md:min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+      <div class="fixed inset-0 h-screen z-10 w-screen overflow-y-auto flex flex-col items-center justify-center" style="height: calc(100vh - 54px);">
+        <div class="p-4 sm:p-0 md:min-h-full md:flex md:flex-col md:items-center md:justify-center">
           <TransitionChild as="template" leave="ease-in duration-200"
             leave-from="opacity-100 translate-y-0 sm:scale-100"
             leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
