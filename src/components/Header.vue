87a29c60858192f0a267e8f254cb5811e911abda
Sergio <sergio.rodrigues@mercoplasa.com.br>
2024-04-05 11:13:49 -0300

    menu mobile mudado

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,36 @@
 <template>
+  <TransitionRoot as="template" :show="mobileMenuOpen">
+    <Dialog as="div" class="relative z-10" @click="fecharMenu">
+      <TransitionChild as="template" enter="ease-in-out duration-500" enter-from="opacity-0" enter-to="opacity-100"
+        leave="ease-in-out duration-500" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+      </TransitionChild>
+
+      <div class="fixed inset-0 overflow-hidden">
+        <div class="absolute inset-0 overflow-hidden">
+          <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+            <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700"
+              enter-from="translate-x-full" enter-to="translate-x-0"
+              leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0"
+              leave-to="translate-x-full">
+              <DialogPanel class="pointer-events-auto relative w-screen max-w-md" style="height: calc(100vh - 65px);">
+                <div class="absolute bottom-0 right-0 w-10/12 flex h-3/6 flex-col overflow-y-scroll bg-white py-6 shadow-xl" style="border-radius: 1rem 0 0 0;">
+                  <div class="flex flex-col justify-center items-end gap-8 mt-6 flex-1 px-4 sm:px-6">
+                    <a href="#about" class="menuLink text-xl font-bold leading-6 text-black" @click="fecharMenu">Sobre mim</a>
+                    <a href="#stacks" class="menuLink text-xl font-bold leading-6 text-black" @click="fecharMenu">Stacks</a>
+                    <a href="#projects" class="menuLink text-xl font-bold leading-6 text-black" @click="fecharMenu">Projetos</a>
+                  </div>
+                </div>
+              </DialogPanel>
+            </TransitionChild>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
   <header class="">
     <nav
-      class="fixed bottom-0 w-screen flex items-center justify-center p-4 lg:px-8 bg-slate-500 z-10 md:top-0 md:h-12 md:bg-black"
+      class="fixed bottom-0 w-screen flex items-center justify-center p-4 lg:px-8 bg-slate-500 z-50 md:top-0 md:h-12 md:bg-black"
       style="border-bottom: 1px solid white;" aria-label="Global">
       <div class="flex lg:flex-1">
         <a href="#" class="-m-1.5 p-1.5">
@@ -10,9 +39,10 @@
       </div>
       <div class="flex md:hidden">
         <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
-          @click="mobileMenuOpen = true">
+          @click="mobileMenuOpen = !mobileMenuOpen">
           <span class="sr-only">Open main menu</span>
-          <Bars3Icon class="h-8 w-8" aria-hidden="true" />
+          <Bars3Icon class="h-8 w-8" aria-hidden="true" v-if="!mobileMenuOpen" />
+          <XMarkIcon class="h-8 w-8" aria-hidden="true" v-else />
         </button>
       </div>
       <PopoverGroup class="hidden md:flex md:gap-x-12">
@@ -21,34 +51,6 @@
         <a href="#projects" class="menuLink text-xl font-bold leading-6 text-white">Projetos</a>
       </PopoverGroup>
     </nav>
-    <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-      <div class="fixed inset-0 z-10" />
-      <DialogPanel
-        class="fixed h-2/4 bottom-0 right-0 z-30 w-4/6 bg-slate-400 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
-        style="border-radius: 1rem 0 0 0;">
-        <div class="fixed bottom-0 right-0 w-screen p-4 flex items-center justify-center bg-slate-500">
-          <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white"
-            @click="mobileMenuOpen = false">
-            <span class="sr-only">Close menu</span>
-            <XMarkIcon class="h-8 w-8" aria-hidden="true" />
-          </button>
-        </div>
-        <div class="mt-6 h-full flex flex-col justify-end items-end">
-          <div class="-my-6 divide-y divide-gray-500/10">
-            <div class="space-y-2 py-6">
-              <div style="display: flex; flex-direction: column; gap: 2rem; padding: 10rem 2rem;">
-                <a href="#about" class="text-xl font-bold leading-6 text-white" @click="mobileMenuOpen = false">Sobre
-                  mim</a>
-                <a href="#stacks" class="text-xl font-bold leading-6 text-white"
-                  @click="mobileMenuOpen = false">Stacks</a>
-                <a href="#projects" class="text-xl font-bold leading-6 text-white"
-                  @click="mobileMenuOpen = false">Projetos</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </DialogPanel>
-    </Dialog>
   </header>
 </template>
 
@@ -57,7 +59,7 @@ import { ref } from 'vue'
 import {
   Dialog,
   DialogPanel,
-  PopoverGroup,
+  PopoverGroup, DialogTitle, TransitionChild, TransitionRoot
 } from '@headlessui/vue'
 import {
   Bars3Icon,
@@ -65,6 +67,14 @@ import {
 } from '@heroicons/vue/24/outline';
 
 const mobileMenuOpen = ref(false)
+
+function abrirMenu() {
+  mobileMenuOpen.value = true
+}
+
+function fecharMenu() {
+  mobileMenuOpen.value = false
+}
 </script>
 
 <style scode>
